3201d7fc24c514dc3f694f1bbdcfa3428a5b5e73
ThierryO <thierry.onkelinx@inbo.be>
2018-12-23 23:57:17 +0100

    add the soundWav class

---
+++ R/soundWav-class.R
@@ -0,0 +1,32 @@
+#' The soundWav class
+#'
+#' Holds the relevant content of wav files
+#' @section Slots:
+#'   \describe{
+#'    \item{\code{Recording}}{The metadata on the recording}
+#'    \item{\code{Values}}{The actual recorded values}
+#'   }
+#' @name soundWav-class
+#' @rdname soundWav-class
+#' @exportClass soundWav
+#' @aliases soundWav-class
+#' @importFrom methods setClass
+#' @docType class
+#' @include soundWavMeta-class.R
+setClass(
+  "soundWav",
+  representation = representation(
+    Values = "integer"
+  ),
+  contains = "soundWavMeta"
+)
+
+#' @importFrom methods setValidity
+#' @importFrom assertthat assert_that
+setValidity(
+  "soundWav",
+  function(object){
+    assert_that(is.vector(object@Values))
+    return(TRUE)
+  }
+)
