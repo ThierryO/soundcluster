bfa293a26d4ce98ab5c19dabb96123c678b6fda6
ThierryO <thierry.onkelinx@inbo.be>
2018-12-24 12:34:17 +0100

    add the soundSpectrogram class

---
+++ R/soundSpectrogram-class.R
@@ -0,0 +1,24 @@
+#' The soundSpectrogram class
+#'
+#' Holds a spectrogram and it's metadata.
+#' @section Slots:
+#'   \describe{
+#'    \item{\code{SpecGram}}{The actual spectrogram}
+#'    \item{\code{Recording}}{A data.frame with fingerprint, filename, timestamp, sample_rate, te_factor, left_channel}
+#'    \item{\code{Spectrogram}}{A data.frame with fingerprint, window_ms, window_n, overlap, recording}
+#'   }
+#' @name soundSpectrogram-class
+#' @rdname soundSpectrogram-class
+#' @exportClass soundSpectrogram
+#' @aliases soundSpectrogram-class
+#' @importFrom methods setClass
+#' @docType class
+#' @include soundSpectrogramMeta-class.R
+#' @include import_S3_classes.R
+setClass(
+  "soundSpectrogram",
+  representation = representation(
+    SpecGram = "specgram"
+  ),
+  contains = "soundSpectrogramMeta"
+)
