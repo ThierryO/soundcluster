0d039724da69905a5c454e541f27dc1f42f191fe
ThierryO <thierry.onkelinx@inbo.be>
2018-12-29 20:56:03 +0100

    wav2rds() gains an existing argument

--- R/wav2rds.R
+++ R/wav2rds.R
@@ -2,6 +2,7 @@
 #'
 #' Read the wav files in a directory, extract the pulses and store them in one rds file per wav file.
 #' @param path the name of a file or a directory
+#' @param existing what to do with existing rds files. Options are `skip`, `append` and `overwrite`.
 #' @inheritParams base::list.files
 #' @inheritParams sound_wav
 #' @inheritParams wav2spectrogram
@@ -12,9 +13,10 @@
 wav2rds <- function(
   path = ".",
   recursive = TRUE,
+  existing = c("skip", "append", "overwrite"),
   threshold_amplitude = 10,
   min_peak_amplitude = 30,
-  dimensions = 32,
+  dimensions = 64,
   channel = c("left", "right"),
   te_factor = 1,
   max_length = 5,
@@ -26,10 +28,15 @@ wav2rds <- function(
     is.flag(recursive)
   )
   channel <- match.arg(channel)
+  existing <- match.arg(existing)
 
   if (file_test("-f", path)) {
     message(path)
     flush.console()
+    target <- gsub("(.*)\\..*?$", "\\1.rds", path)
+    if (existing == "skip" && file_test("-f", target)) {
+      return(NULL)
+    }
     wav <- sound_wav(
       filename = path,
       channel = channel,
@@ -45,7 +52,10 @@ wav2rds <- function(
       min_peak_amplitude = min_peak_amplitude,
       dimensions = dimensions
     )
-    saveRDS(pulses, file = gsub("(.*)\\..*?$", "\\1.rds", path))
+    if (existing == "append" && file_test("-f", target)) {
+      pulses <- rbind(readRDS(target), pulses)
+    }
+    saveRDS(pulses, file = target)
     return(TRUE)
   }
   if (!file_test("-d", path)) {
@@ -57,29 +67,25 @@ wav2rds <- function(
     recursive = recursive,
     full.names = TRUE
   )
-  rds <- gsub("\\.[Ww][Aa][Vv]$", ".rds", wavs)
-  wavs <- wavs[!file.exists(rds)]
+  if (existing == "skip") {
+    rds <- gsub("\\.[Ww][Aa][Vv]$", ".rds", wavs)
+    wavs <- wavs[!file.exists(rds)]
+  }
   if (requireNamespace("littler", quietly = TRUE)) {
     cmds <- sprintf(
       "r -e 'soundcluster::wav2rds(
       path = \"%s\", threshold_amplitude = %f, min_peak_amplitude = %f,
       dimensions = %i, channel = \"%s\", te_factor = %f, max_length = %f,
-      window_ms = %f, overlap = %f)'",
-      wavs,
-      threshold_amplitude = threshold_amplitude,
-      min_peak_amplitude = min_peak_amplitude,
-      dimensions = dimensions,
-      channel = channel,
-      te_factor = te_factor,
-      max_length = max_length,
-      window_ms = window_ms,
-      overlap = overlap
+      window_ms = %f, overlap = %f, existing = \"%s\")'",
+      wavs, threshold_amplitude, min_peak_amplitude, dimensions, channel,
+      te_factor, max_length, window_ms, overlap, existing
     )
     lapply(cmds, system)
   } else {
     lapply(
       wavs,
       wav2rds,
+      existing = existing,
       threshold_amplitude = threshold_amplitude,
       min_peak_amplitude = min_peak_amplitude,
       dimensions = dimensions,
