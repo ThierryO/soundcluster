70a9622ef1a1d9afcd64dd8b68d2e66b08e01102
ThierryO <thierry.onkelinx@inbo.be>
2018-12-31 15:30:39 +0100

    add connect_db()

---
+++ R/connect_db.R
@@ -0,0 +1,82 @@
+#' Connect to the SQLite database
+#'
+#' The database is an SQLite database named `soundcluster.sqlite`. This file will be created if it doesn't exist at the `path`.
+#' @param path the path to the database
+#' @export
+#' @importFrom assertthat assert_that is.string
+#' @importFrom utils file_test
+#' @importFrom RSQLite dbConnect SQLite dbSendQuery dbClearResult
+connect_db <- function(path = ".") {
+  assert_that(
+    is.string(path),
+    file_test("-d", path)
+  )
+
+  db <- file.path(path, "soundcluster.sqlite")
+  connection <- dbConnect(SQLite(), dbname = db)
+  res <- dbSendQuery(
+    connection,
+    "CREATE TABLE IF NOT EXISTS main.device (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      make TEXT NOT NULL,
+      model TEXT NOT NULL,
+      serial TEXT,
+      sample_rate REAL NOT NULL,
+      te_factor REAL NOT NULL,
+      left_channel INTEGER NOT NULL
+    )"
+  )
+  dbClearResult(res)
+  res <- dbSendQuery(
+    connection,
+    "CREATE TABLE IF NOT EXISTS main.recording (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      fingerprint TEXT NOT NULL UNIQUE,
+      timestamp INTEGER NOT NULL,
+      device INTEGER NOT NULL REFERENCES device (id)
+    )"
+  )
+  dbClearResult(res)
+  res <- dbSendQuery(
+    connection,
+    "CREATE TABLE IF NOT EXISTS main.spectrogram (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      fingerprint TEXT NOT NULL UNIQUE,
+      recording INTEGER NOT NULL REFERENCES recording (id),
+      window_ms REAL NOT NULL,
+      window_n REAL NOT NULL,
+      overlap REAL NOT NULL
+    )"
+  )
+  dbClearResult(res)
+  res <- dbSendQuery(
+    connection,
+    "CREATE TABLE IF NOT EXISTS main.pulse (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      fingerprint TEXT NOT NULL UNIQUE,
+      spectrogram INTEGER NOT NULL REFERENCES spectrogram (id),
+      peak_time REAL NOT NULL,
+      peak_frequency REAL NOT NULL,
+      peak_amplitude REAL NOT NULL,
+      start_time REAL NOT NULL,
+      start_frequency REAL NOT NULL,
+      start_amplitude REAL NOT NULL,
+      end_time REAL NOT NULL,
+      end_frequency REAL NOT NULL,
+      select_amplitude REAL NOT NULL
+    )"
+  )
+  dbClearResult(res)
+  res <- dbSendQuery(
+    connection,
+    "CREATE TABLE IF NOT EXISTS main.pyramid (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      pulse INTEGER NOT NULL REFERENCES pulse (id),
+      parent INTEGER REFERENCES pyramide (id),
+      quandrant INTEGER NOT NULL,
+      value REAL NOT NULL
+    )"
+  )
+  dbClearResult(res)
+  return(connection)
+}
