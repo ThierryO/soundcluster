2fefc8ab3298de978305477bd6e415b1c4c0020d
ThierryO <thierry.onkelinx@inbo.be>
2019-01-26 18:27:31 +0100

    add get_node_classification()

---
+++ R/get_node_classification.R
@@ -0,0 +1,82 @@
+#' get the classification for each node in a model
+#'
+#' @param x a soundDatabase
+#' @param model an optional model id
+#' @return a dataframe with the classification
+#' @export
+get_node_classification <- function(x, model) {
+  UseMethod("get_node_classification", x)
+}
+
+#' @export
+get_node_classification.soundDatabase <- function(x, model) {
+  if (missing(model)) {
+    get_node_classification(x = x@Connection)
+  } else {
+    get_node_classification(x = x@Connection, model = model)
+  }
+}
+
+#' @importFrom assertthat assert_that is.number
+#' @importFrom RSQLite dbGetQuery dbQuoteLiteral
+#' @importFrom pool poolCheckout poolReturn
+#' @importFrom stats aggregate
+#' @export
+get_node_classification.Pool <- function(x, model) {
+  conn <- poolCheckout(x)
+  on.exit(poolReturn(conn))
+  if (missing(model)) {
+    sql <-
+      "WITH cte_weight AS (
+        SELECT pr.node, p.class, SUM(1 / pr.distance) AS weight
+        FROM pulse AS p
+        INNER JOIN prediction AS pr ON p.id = pr.pulse
+        WHERE p.class IS NOT NULL
+        GROUP BY pr.node, p.class
+      ),
+      cte_total AS (
+        SELECT node, SUM(weight) AS total_weight
+        FROM cte_weight
+        GROUP BY node
+      )
+
+      SELECT
+        cw.node, cw.class, c.abbreviation,
+        100 * cw.weight / ct.total_weight AS probability
+      FROM cte_weight AS cw
+      INNER JOIN cte_total AS ct ON cw.node = ct.node
+      INNER JOIN class AS c ON cw.class = c.id
+      ORDER BY probability DESC"
+  } else {
+    assert_that(is.number(model))
+    sql <- sprintf(
+      "WITH cte_weight AS (
+        SELECT pr.node, p.class, SUM(1 / pr.distance) AS weight
+        FROM pulse AS p
+        INNER JOIN prediction AS pr ON p.id = pr.pulse
+        INNER JOIN node AS n on pr.node = n.id
+        WHERE p.class IS NOT NULL AND n.model = %s
+        GROUP BY pr.node, p.class
+      ),
+      cte_total AS (
+        SELECT node, SUM(weight) AS total_weight
+        FROM cte_weight
+        GROUP BY node
+      )
+
+      SELECT
+        cw.node, cw.class, c.abbreviation,
+        100 * cw.weight / ct.total_weight AS probability
+      FROM cte_weight AS cw
+      INNER JOIN cte_total AS ct ON cw.node = ct.node
+      INNER JOIN class AS c ON cw.class = c.id
+      ORDER BY probability DESC",
+      dbQuoteLiteral(conn, model)
+    )
+  }
+  class <- dbGetQuery(conn, sql)
+  dominant <- class[!duplicated(class$node), c("node", "class")]
+  class$text <- sprintf("%s (%.0f%%)", class$abbreviation, class$probability)
+  text <- aggregate(text ~ node, class, paste)
+  merge(dominant, text, by = "node")
+}
